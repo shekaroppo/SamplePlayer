4e1dbecb50d323f7361552de5469cb18781b43e1
Chandrasekhar Allam <chandrasekhar.allam@mutualmobile.com>
2016-06-07 13:36:43 +0530

    Initial commit

---
+++ app/src/main/java/shekar/com/sampleplayer/Config.java
@@ -0,0 +1,10 @@
+package shekar.com.sampleplayer;
+
+/**
+ * Created by echessa on 6/17/15.
+ */
+public class Config {
+
+    public static final String CLIENT_ID = "78d7732c01df30378ad6f8a6917ba371";
+    public static final String API_URL = "https://api.soundcloud.com";
+}
